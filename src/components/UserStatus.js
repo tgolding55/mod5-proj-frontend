8333461ff72de7b393a1d90f9b4b4ee62dc288f2
Tim Golding <tgolding55@hotmail.com>
2019-12-11 14:39:27 +0000

    small styling change

--- src/components/UserStatus.js
+++ src/components/UserStatus.js
@@ -1,47 +1,37 @@
 import React from "react";
-import { Grid, Divider, Segment, Button, Card } from "semantic-ui-react";
+import { Segment, Button, Label } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const UserStatus = ({ setUser, setSelectedPage, user }) => {
   return (
     <Segment>
       {user ? (
-        user.github_name ? (
-          <>
-            <Card>
-              <Card.Header>Hello {user.github_name}</Card.Header>
-              <Card.Content>
-                <Button
-                  onClick={() => {
-                    localStorage.removeItem("token");
-                    setUser(null);
-                  }}
-                >
-                  Log out
-                </Button>
-              </Card.Content>
-            </Card>
-          </>
-        ) : (
-          <Button href="http://localhost:3001/auth/github">Github time!</Button>
-        )
+        <>
+          <Button
+            onClick={() => {
+              localStorage.removeItem("token");
+              setUser(null);
+            }}
+          >
+            Log out
+          </Button>
+          {user.github_name ? (
+            <Label>Github Linked</Label>
+          ) : (
+            <Button href="http://localhost:3001/auth/github">
+              Github time!
+            </Button>
+          )}
+        </>
       ) : (
         <>
-          <Grid columns={2} relaxed="very" stackable>
-            <Grid.Column>
-              <Link to="/Login">
-                <Button onClick={() => setSelectedPage("login")}>Login</Button>
-              </Link>
-            </Grid.Column>
-            <Grid.Column>
-              <Link to="/Signup">
-                <Button onClick={() => setSelectedPage("signup")}>
-                  Sign Up
-                </Button>
-              </Link>
-            </Grid.Column>
-          </Grid>
-          <Divider vertical>Or</Divider>
+          <Link to="/Login">
+            <Button onClick={() => setSelectedPage("login")}>Login</Button>
+          </Link>
+
+          <Link to="/Signup">
+            <Button onClick={() => setSelectedPage("signup")}>Sign Up</Button>
+          </Link>
         </>
       )}
     </Segment>
