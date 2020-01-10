b5a85bf7ec2e68fa5bd47c46a27e018fc80b2571
tgolding55 <tgolding55@hotmail.com>
2020-01-10 18:30:32 +0000

    small fixes for build

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Signup from "../components/Signup";
+import Signup from "../components/signup";
 import { Segment } from "semantic-ui-react";
 
 const SignupPage = ({ setUser, setLoading }) => {
